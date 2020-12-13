b9961ac219e8b74eaceb3b5b4f5417466a4d7122
ha382 <ha382@njit.edu>
2020-12-13 10:20:53 -0500

    trying 1 again again aga

--- index.php
+++ index.php
@@ -1,24 +1,38 @@
 <?php
 
+$curl = curl_init();
 
-$url="https://famous-quotes4.p.rapidapi.com/random?count=2&category=all";
-//  Initiate curl
-$ch = curl_init();
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,$url);
-// Execute
-$result=curl_exec($ch);
-// Closing
-curl_close($ch);
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://famous-quotes4.p.rapidapi.com/random?count=2&category=all",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"x-rapidapi-host: famous-quotes4.p.rapidapi.com",
+		"x-rapidapi-key: cac42a454bmsh9b2be104a7413e4p1375bbjsnf2c2f7878e3a"
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+
 
 // Will dump a beauty json :3
-var_dump(json_decode($result, true));
-$result = file_get_contents($url);
-// Will dump a beauty json :3
-var_dump(json_decode($result, true));
+var_dump(json_decode($response, true));
+$response = file_get_contents($curl);
 
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
 
 ?>
 <html>
