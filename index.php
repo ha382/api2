2ca7bbce0ebde79186f41f6daade932e4c6a29c6
ha382 <ha382@njit.edu>
2020-12-15 18:41:04 -0500

    loop cat api

--- index.php
+++ index.php
@@ -10,14 +10,7 @@
           $urlstring = "https://api.thecatapi.com/v1/images/search?breed_id=" . $dataj[$i]->id;
           $cats = json_decode(file_get_contents($urlstring));
         ?>
-        <li>
-          <a href="<?php echo "/breed.php?name=" . $cats[0]->breeds[0]->name; ?>">
-            <div>
-              <?php echo $dataj[$i]->name; ?>
-              <img src="<?php echo $cats[0]->url?>" width="100", height="100"> </img>
-            </div>
-          </a>
-        </li>
+        
       <?php endforeach ?>
     </ul>
   </body>
